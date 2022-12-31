446f75363bce5049c12333615ccaf792fbeddc62
Andrew Tyshchenko (Mac) <andrij.tyshchenko@gmail.com>
2022-12-31 23:59:45 +0200

    Add annotation @WebFilter and move url patterns from method doFilter() inside it

--- hw4/src/main/java/dev/profitsoft/hw4/filters/AuthorizationFilter.java
+++ hw4/src/main/java/dev/profitsoft/hw4/filters/AuthorizationFilter.java
@@ -6,9 +6,9 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
@@ -18,7 +18,7 @@ import java.io.IOException;
  * If session attribute "current_user" is null and requested page is one of the mentioned above,
  * then user will be redirected to the login page. Otherwise, filter gives control to filter chain.
  */
-@Component
+@WebFilter(urlPatterns = {"/homepage", "/users"})
 public class AuthorizationFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -32,9 +32,8 @@ public class AuthorizationFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         Object currentUser = request.getSession().getAttribute("current_user");
-        String requestURI = request.getRequestURI();
 
-        if (currentUser == null && (requestURI.startsWith("/homepage") || requestURI.startsWith("/users"))) {
+        if (currentUser == null) {
             response.sendRedirect("/login");
         } else {
             filterChain.doFilter(servletRequest, servletResponse);
