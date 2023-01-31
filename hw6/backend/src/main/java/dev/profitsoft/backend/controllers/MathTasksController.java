f14f3841c92735ef655db97037194e5db22df845
Andrew Tyshchenko (Mac) <andrij.tyshchenko@gmail.com>
2023-01-31 17:32:54 +0200

    Add module for backend endpoint

---
+++ hw6/backend/src/main/java/dev/profitsoft/backend/controllers/MathTasksController.java
@@ -0,0 +1,26 @@
+package dev.profitsoft.backend.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/math/mathtask")
+public class MathTasksController {
+    @GetMapping()
+    public List<String> getMathTasks(@RequestParam int count) {
+        List<String> mathTasks = new ArrayList<>(count);
+
+        Random rand = new Random();
+
+        String[] operator = {"+", "-", "/", "*"};
+
+        for (int i = 0; i < count; i++) {
+            mathTasks.add(rand.nextInt(100) + operator[rand.nextInt(operator.length)] + rand.nextInt(100));
+        }
+
+        return mathTasks;
+    }
+}
